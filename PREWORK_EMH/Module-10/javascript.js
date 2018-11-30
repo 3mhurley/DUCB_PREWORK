9e0b9ef95612107fccec6122b954acb64bd211f8
EMH <33641969+3mhurley@users.noreply.github.com>
2018-11-29 19:15:03 -0700

    Initialize

---
+++ PREWORK_EMH/Module-10/javascript.js
@@ -0,0 +1,17 @@
+function grow(id) {
+    id.style.height = "250px";
+}
+
+function blueC(id) {
+    id.style.backgroundColor = "blue";
+}
+
+function fade(id) {
+    id.style.opacity = "0.5";
+}
+
+function reset(id) {
+    id.style.height = "150px";
+    id.style.backgroundColor = "orange";
+    id.style.opacity = "1.0";
+}
